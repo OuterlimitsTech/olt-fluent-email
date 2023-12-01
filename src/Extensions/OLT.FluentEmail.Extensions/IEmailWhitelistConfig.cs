9c802d57b7758475215a9c44973181c471f9c404
Chris Straw <chris.straw@outerlimitstech.com>
2023-12-01 14:22:06 -0500

    minor cleanup

---
+++ src/Extensions/OLT.FluentEmail.Extensions/IEmailWhitelistConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace FluentEmail.Core
+{
+    public interface IEmailWhitelistConfig
+    {
+        IEnumerable<string> Domains { get; set; }
+        IEnumerable<string> EmailAddresses { get; set; }
+        bool ProductionMode { get; }
+
+        IFluentEmail TrimEmails(IFluentEmail fluentEmail);
+    }
+
+}
